f04ad66d0d01a8f4fa46a62a1a6a7dd20cbd5411
TejasBiranje0201 <tejasbiranje6717@gmail.com>
2025-03-21 12:35:39 +0530

    eroors solve in backed

--- adpro_backend/src/main/java/com/adpro/entity/WorkSchedule.java
+++ adpro_backend/src/main/java/com/adpro/entity/WorkSchedule.java
@@ -5,6 +5,9 @@ import java.sql.Date;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -21,7 +24,8 @@ import lombok.ToString;
 @ToString
 public class WorkSchedule {
 	
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String title;
 	private String  description;
