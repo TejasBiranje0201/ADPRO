97af03a73d8c2be76cf5ac0c5edcf28592292505
Satyajit Kadam <satyajitkadam@Satyajits-MacBook-Air.local>
2025-04-08 11:42:50 +0530

    change in workSchedule

--- adpro_backend/src/main/java/com/adpro/controller/UserController.java
+++ adpro_backend/src/main/java/com/adpro/controller/UserController.java
@@ -2,7 +2,6 @@ package com.adpro.controller;
 
 import java.util.List;
 
-import org.hibernate.sql.ast.tree.expression.NestedColumnReference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
